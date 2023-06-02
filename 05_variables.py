330ff9e60ed4ebb2a2c261ef4d8f265b44929438
Muhammad Anas <muhammadanas6867@gmail.com>
2023-06-02 10:46:24 -0700

    added variables

---
+++ 05_variables.py
@@ -0,0 +1,28 @@
+# variables: objects containing specfic values
+x=6 # integer or numeric variable
+print(x) 
+
+y="we are learning with youtube" #string variable
+print(y)
+
+x= x+10  #x=5
+print(x)
+
+#type(x) or class of variables
+type(x)
+print(type(x))  #class integar
+type(y)
+print(type(y))  #class string
+
+#CLASS is depend on output
+
+#Rules to assign a variable
+#1-variables should contain a number,letters and underscores.
+#2- donot start with numbers
+#3-spaces are not allowed
+#4-donot use keywards used in python(test,media,)
+#5-short and descriptive
+#6-case sensitivity(lowercase,upper case ,lower case should be used) 
+
+fruit_basket="mangoes,apples"
+print(fruit_basket)
